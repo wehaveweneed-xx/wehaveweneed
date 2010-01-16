01aa784a307363647797e3c7f0bdb05f9edfbf34
Eric Miller <emiller@rita.(none)>
2010-01-16 09:13:40 -0800

    Initial data model.

--- web/models.py
+++ web/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from datetime datetime
 
-# Create your models here.
+POSTCHOICE = (
+	('have', 'Offer')
+	('need', 'Request')
+)
+PRIORITYCHOICE = (
+	('immediate', 'Immediate / Life-Saving')
+	('midterm', 'Mid-Term / Life-Sustaining')
+)
+
+class Category(models.Model):
+	name  = models.CharField(max_length=200)
+
+class UserProfile(models.Model):
+	user 	= models.ForeignKey(User, unique=True)
+	phone = models.CharField(max_length=100)
+
+class Post(models.Model):
+	created_at 		= models.DateTimeField(default=datetime.utcnow)
+	title 				= models.CharField(max_length=200)
+	type 					= models.CharField(max_length=10, choices=POSTCHOICE)
+	priority			= models.CharField(max_length=10, choices=PRIORITYCHOICE)
+	location 			= models.CharField(max_length=100)
+	geostamp 			= models.CharField(max_length=100, blank=True)
+	time_start 		= models.DateTimeField(default=datetime.utcnow, blank=True)
+	time_end 			= models.DateTimeField(blank=True)
+	category 			= models.ForeignKey(Category)
+	contact_name 	= models.CharField(max_length=100)
+	contact_email = models.CharField(max_length=100)
+	contact_phone = models.CharField(max_length=40)
+	user 					= models.ForeignKey(User, blank=True, null=True)
+	content 			= models.TextField()
+
+def __unicode__(self):
+	return self.title
