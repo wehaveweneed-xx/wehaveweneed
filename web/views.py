6bbcbb56e4dfe56bc4a3a07f72e5b7543460ca71
Alan Viars <alan@alan-laptop.(none)>
2010-01-16 17:24:05 -0500

    added views.py

--- web/views.py
+++ web/views.py
@@ -1,56 +1,46 @@
 # Create your views here.
-import os
 from django.utils.translation import ugettext as _
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from wehaveweneed.web.models import *
+from wehaveweneed.web.forms import *
 from django.core import serializers
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.views.decorators.http import require_POST
 from django.core.urlresolvers import reverse
-from wehaveweneed.main.models import *
-from django.contrib.auth.models import User
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from wehaveweneed import settings
 
 @login_required
 def post_create(request):
     
-    if request.method == 'POST': # If the form has been submitted...
-        form = PostForm(request.POST or None)
-        
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            
-            post=form.save()
-            
-                
-            if 'next' in request.POST:
-                    next = request.POST['next']
-            else:
-                    #next = reverse('view_all_schedules')
-                    next = "/haves"
-                    return HttpResponseRedirect(next) # Redirect after POST
+    """
+    Renders a form for creating a new ``POST`` instance, validates against that
+    form, and creates the new instances.
+    """
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.contact = request.user
+        post.save()
+        if 'next' in request.POST:
+            next = request.POST['next']
         else:
-            print 'Attempt to post failed.'
-            error = 'Attempt to post failed.'
-                
-            context = {'form': form, 'error': error}
-                    
-            return render_to_response('post.html',
-                                        context,
-                                        context_instance = RequestContext(request),)
-    else:
-        
-        form = PostForm() # An unbound form
-        data, errors = {}, {}
+            next = reverse('ev_have')
+            
+        request.user.message_set.create(
+                message=_('Your post was created.'))
+        return HttpResponseRedirect(next)
+    
+    if request.is_ajax():
+        raise Http404
     
     return render_to_response(
         'post.html',
         {'form': form},
-        context_instance = RequestContext(request),
+        context_instance = RequestContext(request)
     )
+post_create = login_required(post_create)
     
 def viewhaves(request):
     posts==Post.objects.filter(type="have")
@@ -68,3 +58,4 @@ def viewneeds(request):
         'need.html',
         context,
         context_instance=RequestContext(request),
+    )
