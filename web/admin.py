d5caf5ba262c0f941a8aa7306497491134231faa
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-01-16 12:45:50 -0500

    add some fixes to models and added an admin

---
+++ web/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from wehaveweneed.web.models import Category, Post
+
+admin.site.register(Category)
+admin.site.register(Post)
--- web/models.py
+++ web/models.py
@@ -3,36 +3,45 @@ from datetime import datetime
 from django.contrib.auth.models import User
 
 POSTCHOICE = (
-	('have', 'Offer'),
-	('need', 'Request')
+    ('have', 'Offer'),
+    ('need', 'Request')
 )
 PRIORITYCHOICE = (
-	('immediate', 'Immediate / Life-Saving'),
-	('midterm', 'Mid-Term / Life-Sustaining')
+    ('immediate', 'Immediate / Life-Saving'),
+    ('midterm', 'Mid-Term / Life-Sustaining')
 )
 
 class Category(models.Model):
-	name  = models.CharField(max_length=200)
+    name  = models.CharField(max_length=200)
+    
+    class Meta:
+        ordering = ('name',)
+    
+    def __unicode__(self):
+        return self.name
 
 class UserProfile(models.Model):
-	user 	= models.ForeignKey(User, unique=True)
-	phone = models.CharField(max_length=100)
+    user    = models.ForeignKey(User, unique=True)
+    phone = models.CharField(max_length=100)
 
 class Post(models.Model):
-	created_at 		= models.DateTimeField(default=datetime.utcnow)
-	title 				= models.CharField(max_length=200)
-	type 					= models.CharField(max_length=10, choices=POSTCHOICE)
-	priority			= models.CharField(max_length=10, choices=PRIORITYCHOICE)
-	location 			= models.CharField(max_length=100)
-	geostamp 			= models.CharField(max_length=100, blank=True)
-	time_start 		= models.DateTimeField(default=datetime.utcnow, blank=True)
-	time_end 			= models.DateTimeField(blank=True)
-	category 			= models.ForeignKey(Category)
-	contact_name 	= models.CharField(max_length=100)
-	contact_email = models.CharField(max_length=100)
-	contact_phone = models.CharField(max_length=40)
-	user 					= models.ForeignKey(User, blank=True, null=True)
-	content 			= models.TextField()
+    created_at      = models.DateTimeField(default=datetime.utcnow)
+    title               = models.CharField(max_length=200)
+    type                    = models.CharField(max_length=10, choices=POSTCHOICE)
+    priority            = models.CharField(max_length=10, choices=PRIORITYCHOICE)
+    location            = models.CharField(max_length=100)
+    geostamp            = models.CharField(max_length=100, blank=True)
+    time_start      = models.DateTimeField(default=datetime.utcnow, blank=True)
+    time_end            = models.DateTimeField(blank=True)
+    category            = models.ForeignKey(Category)
+    contact_name    = models.CharField(max_length=100)
+    contact_email = models.CharField(max_length=100)
+    contact_phone = models.CharField(max_length=40)
+    user                    = models.ForeignKey(User, blank=True, null=True)
+    content             = models.TextField()
+    
+    class Meta:
+        ordering = ('-created_at',)
 
-def __unicode__(self):
-	return self.title
+    def __unicode__(self):
+        return self.title
