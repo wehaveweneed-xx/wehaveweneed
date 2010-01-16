a72211293edf1ba871cd0442b5e368825f2dbdf9
Eric Miller <emiller@rita.(none)>
2010-01-16 10:19:42 -0800

    Admin file

---
+++ web/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from wehaveweneed.web.models import UserProfile, Post, Category
+
+admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Category)
