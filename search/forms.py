e47171001d47d5de57e2dbc0dbce226f50bc4a76
Michael Stephens <me@mikej.st>
2010-01-17 11:55:31 +0800

    allow search filtering by 'have' or 'need' - not sure how to make the checkboxes line up properly in the base.html

--- search/forms.py
+++ search/forms.py
@@ -3,6 +3,8 @@ from haystack.forms import SearchForm
 
 class PostSearchForm(SearchForm):
     sources = forms.CharField(required=False)
+    have = forms.BooleanField(initial=True, required=False)
+    need = forms.BooleanField(initial=True, required=False)
 
     def search(self):
         sqs = super(PostSearchForm, self).search()
@@ -10,5 +12,12 @@ class PostSearchForm(SearchForm):
         sources = self.cleaned_data.get('sources')
         if sources and sources != 'all':
             sqs = sqs.filter(category_id=int(sources))
+
+        have = self.cleaned_data['have']
+        need = self.cleaned_data['need']
+        if have and not need:
+            sqs = sqs.filter(type='have')
+        if need and not have:
+            sqs = sqs.filter(type='need')
 
         return sqs
