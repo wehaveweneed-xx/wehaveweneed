28c75e530dc329ffc94616ffb8d56debd41ed748
Michael Stephens <me@mikej.st>
2010-01-20 01:10:21 -0500

    include post url in reply emails and add spaces to WeHaveWeNeed in subject

--- web/emails.py
+++ web/emails.py
@@ -1,15 +1,17 @@
 from django.core.mail import EmailMessage
 
 def send_reply_email(request, post, form):
-    subject = "Someone has replied to '%s' on WeHaveWeNeed" % (
+    subject = "Someone has replied to '%s' on We Have We Need" % (
         post.title)
 
     sender = form.cleaned_data.get('email')
     if not sender:
         sender = request.user.email
         
-    body = "%s said:\n\n%s" % (sender,
-                                    form.cleaned_data['content'])
+    body = "In reference to %s, %s said:\n\n%s" % (
+        request.build_absolute_uri(post.get_absolute_url()),
+        sender,
+        form.cleaned_data['content'])
 
     to = (post.contact.email,)
     headers = {'Reply-To': sender}
