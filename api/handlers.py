e1ba6be04c3ef5cd5650e0dbd516c841e20a7db1
Michael Stephens <me@mikej.st>
2010-01-22 03:35:28 -0500

    include contact organization as well as a link to the post on the site within API results

--- api/handlers.py
+++ api/handlers.py
@@ -1,7 +1,9 @@
+import urlparse
 from piston.handler import BaseHandler
 from piston.utils import rc, validate
+from django.contrib.sites.models import Site
 from wehaveweneed.web.forms import PostForm
-from wehaveweneed.web.models import Category, Post
+from wehaveweneed.web.models import Category, Post, User
 
 class CategoryHandler(BaseHandler):
     """
@@ -11,28 +13,41 @@ class CategoryHandler(BaseHandler):
     fields = ('name','slug')
     model = Category
 
+class ContactHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    fields = ('first_name', 'last_name', 'organization')
+    model = User
+
+    @classmethod
+    def organization(cls, model):
+        return model.get_profile().organization
+
 class PostHandler(BaseHandler):
     """
     Handler for individual posts and listing of posts.
     """
     allowed_methods = ('GET','POST')
-    fields = ('id','type','title','location','priority',
-              ('contact', ('first_name','last_name')),
-              ('category', ('name', 'slug')),
-              'time_start','time_end','created_at','content')
+    fields = ('id', 'type', 'title', 'location', 'priority',
+              'contact', 'category', 'time_start', 'time_end',
+              'created_at', 'content', 'link')
     model = Post
-    
+
+    @classmethod
+    def link(cls, model):
+        base = "http://" + Site.objects.get_current().domain
+        return urlparse.urljoin(base, model.get_absolute_url())
+
     def read(self, request, post_id=None, post_type=None, category=None):
         if post_id:
             return Post.objects.get(pk=post_id)
-        else:            
+        else:
             posts = Post.objects.open()
             if post_type:
                 posts = posts.filter(type=post_type)
             if category:
                 posts = posts.filter(category__slug=category)
             return posts
-    
+
     @validate(PostForm) # validate against post form
     def create(self, request):
         data = request.POST
@@ -51,4 +66,4 @@ class PostHandler(BaseHandler):
         post.save()
         print post.pk
         return rc.CREATED
-       
+
