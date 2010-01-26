6fc000e9feb5e27c93f10a5c7babad3d94352b1b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-01-26 17:57:54 -0500

    add fulfilled class

--- media/scripts/wehaveweneed.js
+++ media/scripts/wehaveweneed.js
@@ -3,7 +3,8 @@ $().ready(function() {
     $('form.toggle_form').bind('submit', function() {
         var form = $(this);
         $.post(form.attr('action'), form.serialize(), function(data) {
-            var fulfilled = form.find('input.fulfilled').val();
+            var fulfilled = form.find('input.fulfilled').val();0
+            form.find('button').toggleClass('fulfilled');
             form.find('button span').html((fulfilled == 'true') ? 'Show Post' : 'Hide Post');
             form.find('input.fulfilled').val((fulfilled == 'true') ? 'false' : 'true');
         });
